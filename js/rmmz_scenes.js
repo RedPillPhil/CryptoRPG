516f448232a181db0f2edaf08e7a4426613ef5ef
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2025-04-04 21:53:32 -0400

    update

--- js/rmmz_scenes.js
+++ js/rmmz_scenes.js
@@ -337,7 +337,7 @@ Scene_Boot.prototype.start = function() {
 Scene_Boot.prototype.startNormalGame = function() {
     this.checkPlayerLocation();
     DataManager.setupNewGame();
-    SceneManager.goto(Scene_Title);
+    SceneManager.goto(Scene_Map);
     Window_TitleCommand.initCommandPosition();
 };
 
